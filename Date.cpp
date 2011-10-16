dacd3325e1a0cea3695b82769be6ecc09ace88c4
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2011-10-16 21:11:51 +0200

    Implemented caching of date information for performance

--- Date.cpp
+++ Date.cpp
@@ -2,35 +2,72 @@
 #include <stdlib.h>
 #include <iostream>
 
-class Date
-{
-protected:
+#define YEAR 0
+#define MONTH 1
+#define DAY 2
 
+class Date {
+
+private:
 	// Store dates as UNIX timestamps
-	time_t date;
+	time_t timestamp;
+
+	void clear_cache() {
+		cache[YEAR] = 0;
+		cache[MONTH] = 0;
+		cache[DAY] = 0;
+	}
+
+protected:
+	int cache[3];
+	virtual void refresh_cache() = 0;
 
 public:
-	
 	// Default constructor sets date to today
-	Date() : date( time(&date) ) {}
-
-	// // Create a date object given a certain Unix timestamp
-	// Date(time_t given_date) : date( given_date ) {}
-
-	// // Copy constructor - copy date from other Date into our own
-	// Date(const Date& other) : date( other.date ) {}
+	Date() : timestamp( time(&timestamp) ) {
+		clear_cache();
+	}
 
 	// Return unix timestamp of this Date
 	time_t get_unix_timestamp() const {
-		return date;
+		return timestamp;
 	}
 
-	// Get year from this.date
-	virtual int year() = 0;
-	// Get month from this.date
-	virtual int month() = 0;
-	// Get day from this.day
-	virtual int day() = 0;
+	Date& set_unix_timestamp(time_t new_timestamp) {
+		timestamp = new_timestamp;
+		clear_cache();
+		return *this;
+	}
+
+	int year() {
+		if(cache[YEAR]) {
+			return cache[YEAR];
+		}
+		else {
+			refresh_cache();
+			return cache[YEAR];	
+		}
+	}
+
+	int month() {
+		if(cache[MONTH]) {
+			return cache[MONTH];
+		}
+		else {
+			refresh_cache();
+			return cache[MONTH];	
+		}
+	}
+	
+	int day() {
+		if(cache[DAY]) {
+			return cache[DAY];
+		}
+		else {
+			refresh_cache();
+			return cache[DAY];	
+		}
+	}	
 
 	friend std::ostream & operator<<(std::ostream & os, Date& date) {
 		return os << date.year() << "-" << date.month() << "-" << date.day();
@@ -51,45 +88,26 @@ public:
 		given_date->tm_mon = month - 1;
 		given_date->tm_mday = day;
 
-		date = mktime(given_date);
+		set_unix_timestamp(mktime(given_date));
 	}
 
-	int year() {
-		struct tm* t = localtime(&date);
-		return (t->tm_year + 1900);
-	}
-
-	int month() {
-		struct tm* t = localtime(&date);
-		return (t->tm_mon + 1);
-	}
-
-	int day() {
-		struct tm* t = localtime(&date);
-		return (t->tm_mday);
+	void refresh_cache(){
+		time_t ts = get_unix_timestamp();
+		struct tm* t = localtime(&ts);
+		
+		cache[YEAR] = t->tm_year + 1900;
+		cache[MONTH] = t->tm_mon + 1;
+		cache[DAY] = t->tm_mday;
 	}	
+		
 };
 
 class Julian : public Date {
 
 public:
 	Julian() : Date() {}
-
-	int julian_day() {
-		return ( date / 86400 ) + 2440587;
-	}
-
-	int year() {
-		return julian_day() / 365;	
-	}
-
-	int month() {
-		return (julian_day() % 365) / 12;
-	}
-
-	int day() {
-		return ((julian_day() % 365) % 12) / 31;
-	}	
+	
+	void refresh_cache() {}	
 };
 
 int main(){
@@ -97,7 +115,6 @@ int main(){
 	std::cout << "Today's Gregorian date is: " << gtoday << std::endl;
 
 	Julian jtoday;
-	std::cout << "Today's Julian day number is: " << jtoday.julian_day() << std::endl;
 	std::cout << "Today's Julian date is: " << jtoday << std::endl;		
 
 	return 0;
