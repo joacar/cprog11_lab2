5d66f3422018c3733b7d3272825a25b3171e07b3
Joakim Carselind <j.carselind@gmail.com>
2011-11-08 11:54:27 +0100

    Made changes. Moved date specific into western date

--- date.h
+++ date.h
@@ -1,80 +1,85 @@
-#ifndef _DATE_
-#define _DATE_
+#ifndef DATE_H
+#define DATE_H
 
 #include <iostream>
 #include <string>
 
-namespace lab2 
-{
+namespace lab2 {
 
 class Date {
-
-private:
-	float julian_day_number;
-
+	
 protected:
 	mutable struct cache_struct {
-		int year,month,day,week_day;
+		// cache_struct(int y, int m, int d, int w_d) :
+		// 	year(-1),
+		// 	month(-1),
+		// 	day(-1),
+		// 	week_day(-1) {}
+
+		// cache_struct() :
+		// 	year(-1),
+		// 	month(-1),
+		// 	day(-1),
+		// 	week_day(-1) {}
+
+		int year, month, day, week_day;	
 	} cache;
-	
-	static const int DAYS_IN_SECS[];
 
-	virtual float date2julian_day_number(int year, int month, int day) = 0;
-	virtual void refresh_cache() const = 0;
-
-	virtual std::string week_day_string(int day) const = 0;
-	virtual std::string month_string(int month) const = 0;
-	virtual int days_in_a_month(int year, int month) const = 0;
-	virtual bool validate_date(int year, int month, int day) const = 0;
+	//struct cache_struct cache;
+	int julian_day_number;
+	void set_julian_day_number(int new_jdn);
 	void clear_cache() const;
+
+	virtual void date2julian_day_number(int year, int month, int day) = 0;
+	virtual void refresh_cache() const = 0;
+	
 
 public:
 	Date();
-	Date(const Date& rhs);
-	Date(Date* dp);
-	Date& operator=(const Date& rhs);
+	Date(const Date& date);
+	Date(int jdn);
 	virtual ~Date();
 
-	float get_julian_day_number() const;
-	Date& set_julian_day_number(float jd);
+	friend std::ostream& operator<<(std::ostream& os, const Date& d);
+
+	/***************
+	*** GETTERS ****
+	****************/
+	inline int mod_julian_day() const { return julian_day_number - 2400001; };	// added point five
+	inline int year() const { refresh_cache(); return cache.year; };
+	inline int month() const { refresh_cache(); return cache.month; };
+	inline int day() const { refresh_cache(); return cache.day; };
 	
-	int mod_julian_day() const;
-	int year() const;
-	int month() const;
-	int day() const;	
-	int week_day() const;
-	std::string week_day_name() const;
-	std::string month_name() const;
-	int days_this_month() const;
+	virtual int week_day() const = 0;
+	virtual int days_this_month() const = 0;
 	virtual int days_per_week() const = 0;
 	virtual int months_per_year() const = 0;
-
-
+	virtual const std::string week_day_name() const = 0;
+	virtual const std::string month_name() const = 0;
+	
 	/***************
 	*** MUTATORS ***
 	****************/
-	Date& add_day(int days);
-	Date& add_month(int months);
-	Date& add_month();
-	Date& add_year(int years);
-	Date& add_year();
+	virtual void add_month(int = 1) = 0;
+	//virtual void add_month() { add_month(1); };
+	virtual void add_year(int = 1) = 0;
+	//virtual void add_year() { add_year(1); };
+	inline Date& operator ++() { set_julian_day_number(++julian_day_number); return *this; };
+	inline Date& operator --() { set_julian_day_number(--julian_day_number); return *this; };
+	inline Date& operator +=(int days) { set_julian_day_number(julian_day_number + days); return *this; };
+	inline Date& operator -=(int days) { set_julian_day_number(julian_day_number - days); return *this; };	
 
-	int operator -(const Date& other) const;
-	Date& operator ++();
-	Date& operator --();
-	Date& operator +=(int days);
-	Date& operator -=(int days);
-	friend std::ostream& operator<<(std::ostream & os, const Date& d);	
+	inline int operator -(const Date& rhs) const { return julian_day_number - rhs.julian_day_number; };
 
 	/***************
-	**COMPARATORS **
+	** COMPARATORS *
 	****************/
-	bool operator ==(const Date& rhs) const;
-	bool operator !=(const Date& rhs) const;
-	bool operator <(const Date& rhs) const;
-	bool operator <=(const Date& rhs) const;
-	bool operator >(const Date& rhs) const;
-	bool operator >=(const Date& rhs) const;
+	inline bool operator ==(const Date& rhs) const {return julian_day_number == rhs.julian_day_number; };
+	inline bool operator !=(const Date& rhs) const {return julian_day_number != rhs.julian_day_number; };
+	inline bool operator <(const Date& rhs) const 	{return julian_day_number < rhs.julian_day_number; };
+	inline bool operator <=(const Date& rhs) const {return julian_day_number <= rhs.julian_day_number; };
+	inline bool operator >(const Date& rhs) const 	{return julian_day_number > rhs.julian_day_number; };
+	inline bool operator >=(const Date& rhs) const {return julian_day_number >= rhs.julian_day_number; };
 };
 
 }
