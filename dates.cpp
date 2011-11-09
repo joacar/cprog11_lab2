9ed489251d6783edba7f3bec95c11f2699bc0656
Joakim Carselind <j.carselind@gmail.com>
2011-11-09 21:07:45 +0100

    added tests

--- dates.cpp
+++ dates.cpp
@@ -14,11 +14,30 @@ int main(){
 	time(&time_now);
 	set_k_time(time_now);
 
-	const Julian& j = new Julian(2319,1,7);
-	const Gregorian& g = Gregorian(j);
-	const Gregorian& g2 = Gregorian(g);
-	const Julian& j2 = Gregorian(g2);
-	cout << j2 << endl;
+	// const Julian& j = new Julian(2319,1,7);
+	// const Gregorian& g = Gregorian(j);
+	// const Gregorian& g2 = Gregorian(g);
+	// const Julian& j2 = Gregorian(g2);
+	// cout << j2 << endl;
 
+	Julian jul(2319,1,7);
+	Gregorian greg(jul);
+
+	Julian jul1(greg);
+	Date &d = jul1;
+
+	Gregorian greg1(d);
+
+	std::cout << d.mod_julian_day() << std::endl;
+	std::cout << jul.mod_julian_day() << std::endl;
+	std::cout << jul1.mod_julian_day() << std::endl;
+	std::cout << greg.mod_julian_day() << std::endl;
+	std::cout << greg1.mod_julian_day() << std::endl;
+
+	std::cout << d << std::endl;
+	std::cout << jul << std::endl;
+	std::cout << jul1 << std::endl;
+	std::cout << greg << std::endl;
+	std::cout << greg1 << std::endl;
 	return 0;
 }
