0eeb685fed3b1810e9e7402772df8e24409d7624
Joakim Carselind <j.carselind@gmail.com>
2011-11-10 10:42:49 +0100

    Passes kattis. Slow (0.44 s) but heck, we pass!

--- calendar.h
+++ calendar.h
@@ -1,38 +1,141 @@
 #ifndef CALENDAR_H
 #define CALENDAR_H
 
+#include "date.h"
+
 #include <iostream>
 #include <string>
 #include <map>
-
-#include "date.h"
+#include <utility>
+#include <stdexcept>
 
 namespace lab2 {
 
 template<class T> 
 class Calendar {
-	private:
-		//typedef typename std::pair<T, std::string> Event;
-		//typedef std::map< Event > Events;
-		//typedef typename std::map< T, std::string > Events;
-
-		T date;
-		std::map< T, std::string > events;
-
-		T& set_default_date(int, int, int);
+		// fails
+		T& set_default_date(int d, int m, int y) {
+			T date_;
+			if(y != -1) {
+				return date_(d, m, y );
+			} 
+			else if(m != -1) {
+				return date_(d, m, date.year() );
+			}
+			else if(d != -1) {
+				return date_(d, date.month(), date.month() );
+			}
+			else {
+				return date;
+			}
+		};
 
 	public:
-		Calendar();
-		template<class U> Calendar (const Calendar<U>& cal);
-		~Calendar();
+		T date;
+		std::multimap<T, std::string> events;
 
-		template<class U> T& operator=(const Calendar<U>& rhs);
+		Calendar() : date(), events() {};
+		template<class U> Calendar (const Calendar<U>& cal) : date( cal.date ), events() {
+			typename std::multimap< U, std::string >::const_iterator it, end;
+			end = cal.events.end();
 
-		template<class U> friend std::ostream& operator<<(std::ostream& os, const Calendar<U>& cal);
+			for(it = cal.events.begin(); it != end; it++) {
+				events.insert( std::pair<U, std::string>(it->first, it->second) );
+			}
+		};
+		~Calendar() {};
 
-		bool set_date(int, int, int);
-		bool add_event(std::string event, int = -1, int = -1, int = -1);
-		bool remove_event(std::string event, int = -1, int = -1, int = -1);
+		template<class U> Calendar& operator=(const Calendar<U>& rhs) {
+			date = rhs.date;
+			events.clear();
+
+			typename std::multimap< U, std::string >::const_iterator it, end;
+			end = rhs.events.end();
+
+			for(it = rhs.events.begin(); it != end; it++) {
+				events.insert( std::pair<T, std::string>(it->first, it->second) );
+			}
+			return *this;
+		};
+
+		friend std::ostream& operator<<(std::ostream& os, const Calendar<T>& cal) {
+			typename std::multimap<T, std::string>::const_iterator it, end;
+			end = cal.events.end();
+
+			for(it = cal.events.begin(); it != end; it++) {
+				if(it->first > cal.date)
+					os << it->first << " : " << it->second << std::endl;	
+			}
+			return os;
+		};
+
+		bool set_date(int y, int m, int d) {
+			try {
+				date = T(y, m, d);
+			} catch(std::out_of_range& e) {
+				return false;
+			}
+			return true;
+		};
+
+		bool add_event(std::string e) {
+			return add_event(e, date.day(), date.month(), date.year());
+		}
+		
+		bool add_event(std::string e, int d) { 
+			return add_event(e, d, date.month(), date.year());
+		};
+		
+		bool add_event(std::string e, int d, int m) {
+			return add_event(e, d, m, date.year()); 
+		};
+		
+		bool add_event(std::string event, int d, int m, int y) {
+			try {
+				T date_ = T(y, m, d);
+
+				typename std::multimap<T, std::string>::iterator it, end;
+				end = events.end();
+				for(it = events.begin(); it != end; it++) {
+					if(it->first == date_ && it->second == event)
+						return false;
+				}
+				events.insert( std::pair<T, std::string>(date_, event) );
+				return true;
+			} catch(std::out_of_range& e) {
+				return false;
+			}
+		};
+
+		bool remove_event(std::string e) {
+			return remove_event(e, date.day(), date.month(), date.year());
+		}
+
+		bool remove_event(std::string e, int d) { 
+			return remove_event(e, d, date.month(), date.year());
+		};
+		
+		bool remove_event(std::string e, int d, int m) {
+			return remove_event(e, d, m, date.year()); 
+		};
+
+		bool remove_event(std::string event, int d, int m, int y) {
+			try {
+				T date_ = T(y, m, d);
+				
+				typename std::multimap<T, std::string>::iterator it, end;
+				end = events.end();
+				for(it = events.begin(); it != end; it++) {
+					if(it->first == date_ && it->second == event) {
+						events.erase(it);
+						return true;
+					}
+				}
+				return false;
+			} catch (std::out_of_range& e) {
+				return false;
+			}
+		};
 };
 
 }
